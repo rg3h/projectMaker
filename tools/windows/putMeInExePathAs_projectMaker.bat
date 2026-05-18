28009115d4479c34ad0a8aac8be11c949da4008b
rg3h <rich.gossweiler@gmail.com>
2026-05-18 18:13:41 -0400

    added node_modules to check in

---
+++ tools/windows/putMeInExePathAs_projectMaker.bat
@@ -0,0 +1,9 @@
+:: @fileoverview runProjectMaker.bat runs a node script that creates a project
+@echo off
+setlocal
+:: %~d0  is the drive
+:: %~p0  is the code's directory
+:: %* are the parameters to the bat file
+:: echo %~d0%~p0src %*
+node %~d0%~p0src %*
+endlocal
